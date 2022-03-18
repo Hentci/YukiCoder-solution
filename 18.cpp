9f53f56890ff63f4afe6357659a4b029218b8eca
Hentci <1222bruce@gmail.com>
2022-03-18 18:42:05 +0800

    ac

---
+++ 18.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define ar array
+#define sz(x) ((int)x.size())
+template <class T,class S> inline bool chmin(T &a,const S &b) {return (a> b? a= b, 1: 0);}
+template <class T,class S> inline bool chmax(T &a,const S &b) {return (a< b? a= b, 1: 0);}
+int main(){
+    ios_base::sync_with_stdio(false),cin.tie(0);
+    string s;
+    cin >> s;
+    for(int i = 0;i < sz(s);i++){
+        int mv = (i + 1) % 26;
+        if(s[i] - mv < 'A') mv -= 26;
+        s[i] -= mv;
+        
+    }
+
+    cout << s << "\n";
+
+    return 0;
+}
