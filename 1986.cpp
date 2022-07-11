dfdea621731efaee1891acdab26863ae0235e752
Hentci <1222bruce@gmail.com>
2022-07-11 16:32:53 +0800

    ac

---
+++ 1986.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define ar array
+#define sz(x) ((int)x.size())
+template <class T,class S> inline bool chmin(T &a,const S &b) {return (a> b? a= b, 1: 0);}
+template <class T,class S> inline bool chmax(T &a,const S &b) {return (a< b? a= b, 1: 0);}
+int main(){
+    ios_base::sync_with_stdio(false),cin.tie(0);
+    int n;
+    cin >> n;
+    // o -> 1
+    // oxo -> 1
+    // 5 oxoxo -> 1
+    cout << (n & 1 ? 1 : n / 2) << "\n";
+
+    return 0;
+}
